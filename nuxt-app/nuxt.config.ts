4fdb76efa19da9075503d0de1884a26a9ac20047
Moritz Kronberger <moritz.kronberger99@gmail.com>
2023-04-13 09:53:54 +0200

    Add initial vuetify setup

--- nuxt-app/nuxt.config.ts
+++ nuxt-app/nuxt.config.ts
@@ -3,6 +3,9 @@ export default defineNuxtConfig({
   // Configure tRPC Nuxt
   // Reference: https://trpc-nuxt-docs.vercel.app/get-started/installation
   build: {
-    transpile: ['trpc-nuxt'],
+    transpile: ['trpc-nuxt', 'vuetify'],
   },
+  // Use Vuetify styles
+  // Reference: https://codybontecou.com/how-to-use-vuetify-with-nuxt-3.html
+  // css: ['vuetify/lib/styles/main.sass'],
 })
