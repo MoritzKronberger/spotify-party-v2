582857b5ed6c7f8a88eec13d00fc0d3d8f2ce660
Martin Kohnle <martin.kohnle@hs-augsburg.de>
2023-04-18 19:35:07 +0200

    Add spotify-auth-pkce frontend

--- nuxt-app/pages/index.vue
+++ nuxt-app/pages/index.vue
@@ -1,33 +1,60 @@
 <script setup lang="ts">
+  import { generateCodeChallenge, generateRandomString } from '../utils/pkce'
   definePageMeta({
     layout: 'default-home',
+  })
+  const config = useRuntimeConfig()
+  const spotifyURL = ref('')
+  const clientId = config.public.SPOTIFY_CLIENT_ID
+  const redirectUri = config.public.SPOTIFY_CLIENT_REDIRECT_URL
+  const codeVerifier = generateRandomString(128)
+
+  onMounted(() => {
+    generateCodeChallenge(codeVerifier).then((codeChallenge) => {
+      const state = generateRandomString(16)
+      const scope = 'user-read-private user-read-email'
+
+      localStorage.setItem('code_verifier', codeVerifier)
+
+      const args = new URLSearchParams({
+        response_type: 'code',
+        client_id: clientId,
+        scope,
+        redirect_uri: redirectUri,
+        state,
+        code_challenge_method: 'S256',
+        code_challenge: codeChallenge,
+      })
+      spotifyURL.value = 'https://accounts.spotify.com/authorize?' + args
+      console.log(args)
+    })
   })
 </script>
 
 <template>
-  <v-container class="fill-height flex-column">
-    <v-spacer />
-    <v-row style="min-width: 300px">
-      <v-col>
+  <VContainer class="fill-height flex-column">
+    <VSpacer />
+    <VRow style="min-width: 300px">
+      <VCol>
         <placeholder-card text="Logo" />
-      </v-col>
-    </v-row>
+      </VCol>
+    </VRow>
 
-    <v-row style="min-width: 300px">
-      <v-col>
-        <v-row>
-          <v-col>
-            <button-primary to="/party/create">I am a host</button-primary>
-          </v-col>
-        </v-row>
-        <v-row>
-          <v-col>
+    <VRow style="min-width: 300px">
+      <VCol>
+        <VRow>
+          <VCol>
+            <button-primary :href="spotifyURL">I am a host</button-primary>
+          </VCol>
+        </VRow>
+        <VRow>
+          <VCol>
             <button-secondary>I am a guest</button-secondary>
-          </v-col>
-        </v-row>
-      </v-col>
-    </v-row>
+          </VCol>
+        </VRow>
+      </VCol>
+    </VRow>
 
-    <v-spacer />
-  </v-container>
+    <VSpacer />
+  </VContainer>
 </template>
