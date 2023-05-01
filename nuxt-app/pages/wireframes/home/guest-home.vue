c5de680d51d71f430b06eb89d9e5b822aa62b17d
AminYacine <78700458+AminYacine@users.noreply.github.com>
2023-05-01 20:19:35 +0200

    created home views

---
+++ nuxt-app/pages/wireframes/home/guest-home.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+  definePageMeta({
+    layout: 'wireframes',
+  })
+</script>
+
+<template>
+  <v-container class="fill-height flex-column">
+    <v-spacer />
+
+    <v-row>
+      <v-col>
+        <h1>My Parties</h1>
+      </v-col>
+    </v-row>
+
+    <v-row>
+      <v-col>
+        <v-row>
+          <v-col>
+            <p class="text-center text-subtitle-1 font-weight-bold">Welcome user</p>
+            <p class="text-center">Get started by joining a new party.</p>
+          </v-col>
+        </v-row>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col>
+        <v-btn variant="tonal">Join Party</v-btn>
+      </v-col>
+    </v-row>
+    <v-spacer />
+  </v-container>
+</template>
---
+++ nuxt-app/pages/wireframes/home/host-home.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+  definePageMeta({
+    layout: 'wireframes',
+  })
+</script>
+
+<template>
+  <v-container class="fill-height flex-column">
+    <v-spacer />
+
+    <v-row>
+      <v-col>
+        <h1>My Parties</h1>
+      </v-col>
+    </v-row>
+
+    <v-row>
+      <v-col>
+        <v-row>
+          <v-col>
+            <p class="text-center text-subtitle-1 font-weight-bold">Welcome user</p>
+            <p class="text-center text-body-1">Let your friends control your music.</p>
+            <p class="text-center">Get started by opening a new party.</p>
+          </v-col>
+        </v-row>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col>
+        <v-btn variant="tonal">New Party</v-btn>
+      </v-col>
+    </v-row>
+    <v-spacer />
+  </v-container>
+</template>
