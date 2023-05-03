577093336e9309f1ae8d09ff908216b0c7151080
Moritz Kronberger <moritz.kronberger99@gmail.com>
2023-05-03 09:01:46 +0200

    Add party table, close #18

--- nuxt-app/db/schema/index.ts
+++ nuxt-app/db/schema/index.ts
@@ -1,8 +1,9 @@
 import image from './image'
+import party from './party'
 import songs from './songs'
 
 /**
  * Register all schema components by exporting them from here.
  * (For compatibility with Drizzle `db-push`)
  */
-export { songs, image }
+export { songs, image, party }
