0c5a1bc9d523f2c7a6f84ac1555e9ea619ee6811
Moritz Kronberger <moritz.kronberger99@gmail.com>
2023-05-03 09:14:58 +0200

    Add update party tRPC procedure

--- nuxt-app/server/trpc/routers/party.ts
+++ nuxt-app/server/trpc/routers/party.ts
@@ -1,4 +1,5 @@
 import { createInsertSchema } from 'drizzle-zod'
+import { and, eq } from 'drizzle-orm/expressions'
 import { z } from 'zod'
 import { customAlphabet } from 'nanoid'
 import { router } from '../trpc'
@@ -6,7 +7,8 @@ import { privateProcedure } from '../middleware/auth'
 import { db } from '~/db'
 import party from '~/db/schema/party'
 import image from '~/db/schema/image'
-import { insertId } from '~/server/utils/db'
+import { insertId, rowsAffected } from '~/server/utils/db'
+import { nanoId } from '~/utils/nanoId/zod'
 
 // Create Zod schemas from drizzle schemas.
 const partySchema = createInsertSchema(party).omit({ id: true, userId: true, code: true, imageId: true })
@@ -23,6 +25,18 @@ const generateRandomPartyCode = () => {
   const alphabet = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'
   const nanoid = customAlphabet(alphabet, 6)
   return nanoid()
+}
+
+/** Get id of party image if exists. */
+const getPartyImageId = async (partyId: string, userId: string) => {
+  return (
+    (
+      await db
+        .select()
+        .from(party)
+        .where(and(eq(party.id, partyId), eq(party.userId, userId)))
+    )[0]?.imageId ?? undefined
+  )
 }
 
 export const partyRouter = router({
@@ -45,6 +59,42 @@ export const partyRouter = router({
       const { id, idOnSuccess } = insertId()
       const res = await db.insert(party).values({ ...partyData, id, userId, code: generateRandomPartyCode(), imageId })
       // Return party Id if it was created successfully
-      return idOnSuccess(res)
+      return { id: idOnSuccess(res) }
+    }),
+  /** Update existing party (and image). (Returns undefined for id does not exist or user is not owner of the party). */
+  updateParty: privateProcedure
+    .input(z.object({ data: z.object({ party: partySchema.partial(), image: imageSchema.optional() }), id: nanoId }))
+    .mutation(async ({ ctx, input }) => {
+      // Unpack input data
+      const { party: partyData, image: imageData } = input.data
+      const { id: partyId } = input
+      const userId = ctx.user.id
+      let imageId: string | undefined
+      // Update image (if image data was provided)
+      if (imageData) {
+        // Get id of current party image if exists
+        imageId = await getPartyImageId(partyId, userId)
+        // If no image exists for current party, create new one and get its Id
+        if (!imageId) {
+          const { id, idOnSuccess } = insertId()
+          // TODO: Check image size before inserting into DB!
+          const insertImageRes = await db.insert(image).values({ ...imageData, id, userId })
+          imageId = idOnSuccess(insertImageRes)
+        } else {
+          // Otherwise update the existing image
+          // TODO: Check image size before updating DB!
+          await db
+            .update(image)
+            .set(imageData)
+            .where(and(eq(image.id, imageId), eq(image.userId, userId)))
+        }
+      }
+      // Update party (and image Id if new image was created successfully)
+      const res = await db
+        .update(party)
+        .set(imageData ? { ...partyData, imageId } : partyData)
+        .where(and(eq(party.id, partyId), eq(party.userId, userId)))
+      // Return party Id if update was successful
+      return { id: rowsAffected(res) ? partyId : undefined }
     }),
 })
