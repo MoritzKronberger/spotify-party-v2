76dc8442cee3f55fb16783be32adfb525fe4fe51
Moritz Kronberger <moritz.kronberger99@gmail.com>
2023-05-03 09:00:13 +0200

    Add image table, close #20

--- nuxt-app/db/schema/index.ts
+++ nuxt-app/db/schema/index.ts
@@ -1,7 +1,8 @@
+import image from './image'
 import songs from './songs'
 
 /**
  * Register all schema components by exporting them from here.
  * (For compatibility with Drizzle `db-push`)
  */
-export { songs }
+export { songs, image }
