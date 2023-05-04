c73ff6502c482ea07251e2349ce7c2f3122e6fa1
AminYacine <78700458+AminYacine@users.noreply.github.com>
2023-05-04 13:07:07 +0200

    feat(home pages): created guest login

---
+++ nuxt-app/pages/wireframes/login-guest.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+  definePageMeta({
+    layout: 'wireframes',
+  })
+</script>
+
+<template>
+  <v-container class="fill-height flex-column">
+    <v-spacer />
+
+    <v-row>
+      <v-col>
+        <h1>Choose a name</h1>
+      </v-col>
+    </v-row>
+
+    <v-row>
+      <v-col>
+        <v-form style="min-width: 300px">
+          <v-row>
+            <v-col>
+              <v-text-field label="Guest name" />
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-col>
+              <button-primary>Continue</button-primary>
+            </v-col>
+          </v-row>
+        </v-form>
+      </v-col>
+    </v-row>
+    <v-btn variant="text">Login instead</v-btn>
+    <v-spacer />
+  </v-container>
+</template>
