e7ba23c439b10fab619627b00a0b5a82b1b04d96
AminYacine <78700458+AminYacine@users.noreply.github.com>
2023-05-12 23:43:59 +0200

    created guest list page

---
+++ nuxt-app/pages/wireframes/guest-list.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+  import '@vuepic/vue-datepicker/dist/main.css'
+  import { ref } from 'vue'
+  const guestList = ref([
+    { username: 'username' },
+    { username: 'username' },
+    { username: 'username' },
+    { username: 'username' },
+  ])
+  definePageMeta({
+    layout: 'wireframes',
+  })
+</script>
+
+<template>
+  <v-container class="flex-column">
+    <v-row>
+      <v-col>
+        <p>Guests</p>
+      </v-col>
+      <v-col>
+        <v-btn variant="tonal" prepend-icon="mdi-account-multiple" rounded="xl">1/5</v-btn>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col>
+        <v-btn variant="tonal">invite</v-btn>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-list lines="one" style="height: 300px" class="overflow-y-auto mx-auto">
+        <v-list-item v-for="(guest, index) in guestList" :key="index" :title="guest.username" />
+      </v-list>
+    </v-row>
+  </v-container>
+</template>
