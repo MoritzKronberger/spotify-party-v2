d104e9593f8878618cb4c7dc1b4ae6934b92bdea
Martin Kohnle <martin.kohnle@hs-augsburg.de>
2023-05-16 11:30:24 +0200

    Remove inferRouterOutputs from types trpc

--- nuxt-app/types/trpc.ts
+++ nuxt-app/types/trpc.ts
@@ -1,8 +1,6 @@
-import { inferRouterInputs, inferRouterOutputs } from '@trpc/server'
+import { inferRouterInputs } from '@trpc/server'
 import { AppRouter } from '~/server/trpc/routers'
 
 type RouterInputs = inferRouterInputs<AppRouter>
-type RouterOutputs = inferRouterOutputs<AppRouter>
-export type SongInput = RouterInputs['addSong']
-export type Song = RouterOutputs['getSongs'][number]
+// type RouterOutputs = inferRouterOutputs<AppRouter>
 export type MimeType = NonNullable<RouterInputs['party']['createParty']['image']>['mimeType']
