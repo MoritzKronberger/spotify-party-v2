2a54ec42575e3031136f9d3bbd70543897b6e8d5
Martin Kohnle <martin.kohnle@hs-augsburg.de>
2023-05-16 11:10:53 +0200

    Add MimeType

--- nuxt-app/types/trpc.ts
+++ nuxt-app/types/trpc.ts
@@ -3,6 +3,6 @@ import { AppRouter } from '~/server/trpc/routers'
 
 type RouterInputs = inferRouterInputs<AppRouter>
 type RouterOutputs = inferRouterOutputs<AppRouter>
-
 export type SongInput = RouterInputs['addSong']
 export type Song = RouterOutputs['getSongs'][number]
+export type MimeType = NonNullable<RouterInputs['party']['createParty']['image']>['mimeType']
