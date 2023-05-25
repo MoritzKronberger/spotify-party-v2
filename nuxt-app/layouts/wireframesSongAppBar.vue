c5d9a3f25edee463df8dc2d07b44d8ddc315928e
AminYacine <78700458+AminYacine@users.noreply.github.com>
2023-05-26 01:07:31 +0200

    created new layout with full appbar

---
+++ nuxt-app/layouts/wireframesSongAppBar.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+  import AppWireframes from '~/components/layout/appWireframes'
+  definePageMeta({
+    layout: 'wireframes',
+  })
+  const currentSong = { title: 'song title', artist: 'song artist', image: 'https://vuejs.org/images/logo.png' }
+  const like = () => undefined
+</script>
+<template>
+  <AppWireframes :song="currentSong" :like="like" show-option>
+    <slot />
+  </AppWireframes>
+</template>
+
+<!--<template>-->
+<!--  <v-theme-provider theme="wireframesTheme">-->
+<!--    <v-app class="gradient-background">-->
+<!--      <v-app-bar density="default" color="primary">-->
+<!--        <template #prepend>-->
+<!--          <v-app-bar-nav-icon icon="mdi-arrow-left" @click="useRouter().back()" />-->
+<!--        </template>-->
+<!--        <v-app-bar-title>-->
+<!--          <playing-song :current-song="currentSong" :like="like"></playing-song>-->
+<!--        </v-app-bar-title>-->
+<!--        <template #append>-->
+<!--          <v-btn icon>-->
+<!--            <v-icon>mdi-cog</v-icon>-->
+<!--          </v-btn>-->
+<!--        </template>-->
+<!--      </v-app-bar>-->
+<!--      <v-main>-->
+<!--        <slot />-->
+<!--      </v-main>-->
+<!--    </v-app>-->
+<!--  </v-theme-provider>-->
+<!--</template>-->
