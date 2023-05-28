96ecb6a5b625f6bb4b77d3cb5a56a345f9c48f59
Martin Kohnle <martin.kohnle@hs-augsburg.de>
2023-05-28 13:12:51 +0200

    Update appDefault layout

--- nuxt-app/components/layout/appDefault.vue
+++ nuxt-app/components/layout/appDefault.vue
@@ -1,37 +1,49 @@
 <script setup lang="ts">
+  import PlayingSong from '~/components/playing-song.vue'
   const props = defineProps<{
     hideNavigation?: boolean
+    song?: { title: string; artist: string; image: string }
+    like?: () => void
+    showOption?: boolean
   }>()
 </script>
 
 <template>
   <v-theme-provider theme="defaultTheme" with-background>
     <v-app class="gradient-background">
-      <!-- Optional top app-bar -->
-      <v-app-bar v-if="!props.hideNavigation" title="Spotify Party" style="background: none" />
+      <v-app-bar v-if="!props.hideNavigation" color="primary">
+        <v-app-bar-nav-icon icon="mdi-arrow-left" @click="useRouter().back()" />
+        <v-app-bar-title v-if="song && like">
+          <playing-song :current-song="song" :like="like"></playing-song>
+        </v-app-bar-title>
+        <template v-if="showOption" #append>
+          <v-btn icon>
+            <v-icon>mdi-cog</v-icon>
+          </v-btn>
+        </template>
+      </v-app-bar>
       <!-- App content goes here -->
       <v-main>
         <slot />
       </v-main>
       <!-- Optional bottom navigation -->
-      <v-bottom-navigation v-if="!props.hideNavigation" style="background: none">
-        <v-btn>
-          <v-icon>mdi-location-enter</v-icon>
-          Join Party
-        </v-btn>
-        <v-btn>
-          <v-icon>mdi-party-popper</v-icon>
-          My Party
-        </v-btn>
-        <v-btn>
-          <v-icon>mdi-account</v-icon>
-          Spotify Connect
-        </v-btn>
-      </v-bottom-navigation>
+      <!--      <v-bottom-navigation v-if="!props.hideNavigation">-->
+      <!--        <v-btn>-->
+      <!--          <v-icon>mdi-location-enter</v-icon>-->
+      <!--          Join Party-->
+      <!--        </v-btn>-->
+      <!--        <v-btn>-->
+      <!--          <v-icon>mdi-party-popper</v-icon>-->
+      <!--          My Party-->
+      <!--        </v-btn>-->
+      <!--        <v-btn>-->
+      <!--          <v-icon>mdi-account</v-icon>-->
+      <!--          Spotify Connect-->
+      <!--        </v-btn>-->
+      <!--      </v-bottom-navigation>-->
     </v-app>
   </v-theme-provider>
 </template>
-
 <style scoped>
   /* Use theme custom properties */
   .gradient-background {
