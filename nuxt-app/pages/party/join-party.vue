66c7c2545b438a8eddc1fce92b570b4bd8db63e8
Martin Kohnle <martin.kohnle@hs-augsburg.de>
2023-05-29 19:47:59 +0200

    Add joinparty function

--- nuxt-app/pages/party/join-party.vue
+++ nuxt-app/pages/party/join-party.vue
@@ -2,8 +2,31 @@
   import SvgIcon from '@jamescoyle/vue-icon'
   import { mdiQrcodeScan } from '@mdi/js'
   import SpotButton from '~/components/spot-button.vue'
-
+  // Get tRPC client
+  const nuxtApp = useNuxtApp()
+  const router = useRouter()
+  const $client = nuxtApp.$client
   const path = mdiQrcodeScan
+
+  const partyCode = ref('')
+
+  const joinParty = () => {
+    /* Check if code respectively party is existing */
+    $client.party.checkPartyExists
+      .useQuery({ code: partyCode.value })
+      .then((result) => {
+        if (result.data.value?.exists) {
+          const code: any = partyCode.value
+          router.push({ path: `/party/session`, query: { code }, replace: true })
+        } else {
+          /* AMIN -> message log in UI */
+          console.log('This party doesnt exist!')
+        }
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }
 </script>
 
 <template>
@@ -37,10 +60,10 @@
           <v-col>
             <v-row>
               <v-col>
-                <v-text-field label="Enter Code" />
+                <v-text-field v-model="partyCode" label="Enter Code" />
               </v-col>
             </v-row>
-            <spot-button :primary="true" title="join" />
+            <spot-button :primary="true" title="join" @click="joinParty" />
           </v-col>
         </v-row>
       </v-col>
