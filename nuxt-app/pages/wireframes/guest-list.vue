5212d49524040c1064d05bf3ae5e9014a7ead160
AminYacine <78700458+AminYacine@users.noreply.github.com>
2023-05-29 17:56:08 +0200

    finalized styling

--- nuxt-app/pages/wireframes/guest-list.vue
+++ nuxt-app/pages/wireframes/guest-list.vue
@@ -1,5 +1,4 @@
 <script setup lang="ts">
-  import '@vuepic/vue-datepicker/dist/main.css'
   import { ref } from 'vue'
   const guestList = ref([
     { username: 'username', role: 'Gastgeber' },
@@ -14,31 +13,36 @@
 
 <template>
   <v-container class="flex-column">
-    <v-row>
-      <v-col>
-        <p>Guests</p>
+    <v-card height="100%" style="padding-bottom: 23vh">
+      <v-row>
+        <v-col>
+          <p>Guests</p>
+        </v-col>
+        <v-col class="text-right">
+          <v-btn variant="tonal" prepend-icon="mdi-account-multiple" rounded="xl">1/5</v-btn>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col>
+          <v-list lines="two" style="height: 50vh" class="overflow-y-auto mx-auto">
+            <v-list-item
+              v-for="(guest, index) in guestList"
+              :key="index"
+              :title="guest.username"
+              :subtitle="guest.role"
+            >
+              <template #prepend>
+                <v-avatar color="grey-lighten-1">
+                  <v-icon color="white">mdi-account</v-icon>
+                </v-avatar>
+              </template>
+            </v-list-item>
+          </v-list>
+        </v-col>
+      </v-row>
+      <v-col style="bottom: 0; position: absolute" class="text-end">
+        <v-btn variant="tonal" class="mt-auto">invite</v-btn>
       </v-col>
-      <v-col>
-        <v-btn variant="tonal" prepend-icon="mdi-account-multiple" rounded="xl">1/5</v-btn>
-      </v-col>
-    </v-row>
-    <v-row>
-      <v-col>
-        <v-btn variant="tonal">invite</v-btn>
-      </v-col>
-    </v-row>
-    <v-row>
-      <v-col>
-        <v-list lines="two" style="height: 300px" class="overflow-y-auto mx-auto">
-          <v-list-item v-for="(guest, index) in guestList" :key="index" :title="guest.username" :subtitle="guest.role">
-            <template #prepend>
-              <v-avatar color="grey-lighten-1">
-                <v-icon color="white">mdi-account</v-icon>
-              </v-avatar>
-            </template>
-          </v-list-item>
-        </v-list>
-      </v-col>
-    </v-row>
+    </v-card>
   </v-container>
 </template>
