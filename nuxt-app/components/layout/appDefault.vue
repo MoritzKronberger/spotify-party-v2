e64bc0b580e8b3a95dabf54ed1a2c7f276386a9c
Martin Kohnle <martin.kohnle@hs-augsburg.de>
2023-05-30 15:16:17 +0200

    Update defaultApp

--- nuxt-app/components/layout/appDefault.vue
+++ nuxt-app/components/layout/appDefault.vue
@@ -2,17 +2,24 @@
   import PlayingSong from '~/components/playing-song.vue'
   const props = defineProps<{
     hideNavigation?: boolean
+    routeLocation?: string
     song?: { title: string; artist: string; image: string }
     like?: () => void
     showOption?: boolean
   }>()
+
+  const router = useRouter()
+
+  const routeBack = (location: string | undefined) => {
+    router.push({ path: location || '/', replace: true })
+  }
 </script>
 
 <template>
   <v-theme-provider theme="defaultTheme" with-background>
     <v-app class="gradient-background">
       <v-app-bar v-if="!props.hideNavigation" color="primary">
-        <v-app-bar-nav-icon icon="mdi-arrow-left" @click="useRouter().back()" />
+        <v-app-bar-nav-icon icon="mdi-arrow-left" @click="routeBack(routeLocation)" />
         <v-app-bar-title v-if="song && like">
           <playing-song :current-song="song" :like="like"></playing-song>
         </v-app-bar-title>
