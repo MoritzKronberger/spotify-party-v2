c7ee43f3f489fef902c99010f25558f4f28afba1
AminYacine <78700458+AminYacine@users.noreply.github.com>
2023-06-03 22:52:46 +0200

    created dialog component

---
+++ nuxt-app/components/pop-up-dialog.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+  import SpotButton from '~/components/spot-button.vue'
+  const props = defineProps<{
+    infoText: string
+    onPrimary: () => void
+    onSecondary: () => void
+    primaryText: string
+    title?: string
+  }>()
+  const showDialog = ref(false)
+  const primaryAction = () => {
+    props.onPrimary()
+    showDialog.value = false
+  }
+  const secondaryAction = () => {
+    props.onSecondary()
+    showDialog.value = false
+  }
+</script>
+<template>
+  <v-dialog v-model="showDialog" activator="parent" min-width="30vh">
+    <v-card>
+      <v-card-title v-if="props.title">{{ props.title }}</v-card-title>
+      <v-card-text>{{ props.infoText }}</v-card-text>
+      <v-card-actions>
+        <div class="d-flex">
+          <spot-button :title="props.primaryText" primary @click="primaryAction"></spot-button>
+          <spot-button title="cancel" primary @click="secondaryAction"></spot-button>
+        </div>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
