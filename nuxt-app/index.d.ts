53882bce5934b19ecd76f35775e01ce562cbc828
Moritz Kronberger <moritz.kronberger99@gmail.com>
2023-06-16 08:33:33 +0200

    Add reference

--- nuxt-app/index.d.ts
+++ nuxt-app/index.d.ts
@@ -1,9 +1,12 @@
 import { createTRPCNuxtClient } from 'trpc-nuxt/client'
 import { AppRouter } from './server/trpc/routers'
 
+// Fixes unknown type of app.$client
+// Reference: https://github.com/wobsoriano/trpc-nuxt/issues/91
 declare module '#app' {
   interface NuxtApp {
     $client: ReturnType<typeof createTRPCNuxtClient<AppRouter>>
   }
 }
+
 export {}
