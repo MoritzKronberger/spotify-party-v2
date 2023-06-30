a3a11db5db3268de605feb218331abb24e700a20
Martin Kohnle <martin.kohnle@hs-augsburg.de>
2023-06-30 13:00:30 +0200

    Update spot-button to handle event

--- nuxt-app/components/spot-button.vue
+++ nuxt-app/components/spot-button.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+  import { defineProps, defineEmits } from 'vue'
+
   const props = defineProps<{
     title: string
     primary?: boolean
@@ -6,11 +8,21 @@
     to?: string
     isHref?: boolean
   }>()
+
+  const emits = defineEmits(['click'])
+
+  const handleClick = () => {
+    emits('click')
+  }
 </script>
 
 <template>
-  <v-btn v-if="props.primary && !isHref" block color="primary" :to="props.to">{{ props.title }}</v-btn>
+  <v-btn v-if="props.primary && !isHref" block color="primary" :to="props.to" @click="handleClick">{{
+    props.title
+  }}</v-btn>
   <v-btn v-if="props.primary && isHref" block color="primary" :href="props.to">{{ props.title }}</v-btn>
-  <v-btn v-if="props.error" block color="error" :to="props.to">{{ props.title }}</v-btn>
-  <v-btn v-if="!props.primary && !props.error" variant="outlined" block :to="props.to">{{ props.title }}</v-btn>
+  <v-btn v-if="props.error" block color="error" :to="props.to" @click="handleClick">{{ props.title }}</v-btn>
+  <v-btn v-if="!props.primary && !props.error" variant="outlined" block :to="props.to" @click="handleClick">{{
+    props.title
+  }}</v-btn>
 </template>
