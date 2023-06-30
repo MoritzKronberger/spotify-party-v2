5f71d41354d90af650664e778f823d0ca6fcbc84
Martin Kohnle <martin.kohnle@hs-augsburg.de>
2023-06-30 13:30:37 +0200

    Update host-home

--- nuxt-app/pages/home/host-home.vue
+++ nuxt-app/pages/home/host-home.vue
@@ -50,8 +50,7 @@
       })
   }
 
-  const deletePartyByID = async (event: Event, partyID: string) => {
-    event.stopPropagation()
+  const deletePartyByID = async (partyID: string) => {
     await $client.party.deleteParty
       .mutate({ id: partyID })
       .then(() => {
@@ -62,47 +61,70 @@
         console.log(error)
       })
   }
+
+  const showDialog = ref(false)
+  const edit = ref(false)
 </script>
 
 <template>
   <v-container class="fill-height flex-column">
     <v-spacer />
+
     <v-row>
       <v-col>
-        <h1>My Parties</h1>
+        <h1 class="text-primary">My Parties</h1>
       </v-col>
     </v-row>
     <v-row>
       <v-col>
         <v-row>
           <v-col>
-            <p class="text-center text-subtitle-1 font-weight-bold">
-              Welcome {{ userSpotify.data.value?.display_name }}
-            </p>
-          </v-col>
-        </v-row>
-        <v-row>
-          <v-col v-if="!userParties.data.value">
+            <p class="text-center text-subtitle-1 font-weight-bold">Welcome user</p>
             <p class="text-center text-body-1">Let your friends control your music.</p>
             <p class="text-center">Get started by opening a new party.</p>
           </v-col>
         </v-row>
       </v-col>
     </v-row>
+
     <v-col>
-      <v-card v-if="userParties.data.value?.length">
-        <v-list lines="one" style="height: 300px" class="overflow-y-auto mx-auto">
-          <v-list-subheader>All Parties</v-list-subheader>
+      <v-card elevation="3">
+        <v-col class="align-self-start">
+          <v-row>
+            <v-col cols="8" align-self="center" class="pb-0">
+              <v-list-subheader>All Parties</v-list-subheader>
+            </v-col>
+            <v-col cols="4" class="text-right pb-0">
+              <v-btn v-if="!edit" icon="mdi-pencil" @click="edit = !edit"></v-btn>
+              <v-btn v-else icon="mdi-pencil" color="primary" @click="edit = !edit"></v-btn>
+            </v-col>
+            <v-col class="pt-0"><v-divider /></v-col>
+          </v-row>
+        </v-col>
+        <v-list lines="one" height="40vh" class="overflow-y-auto mx-auto">
           <v-list-item
             v-for="party in userParties.data.value"
             :key="party.id"
-            :title="party.name + ' ' + party.code"
+            :title="party.name"
             :subtitle="party.description ? party.description : ''"
             @click="joinPartyByID(party.id)"
           >
-            <template #append>
-              <v-btn icon="mdi-delete" @click="deletePartyByID($event, party.id)"></v-btn>
+            <template #prepend>
+              <v-avatar :image="party.image" rounded="0"></v-avatar>
             </template>
+            <template #append>
+              <v-list-item-subtitle>{{ party.status }}</v-list-item-subtitle>
+              <v-btn v-if="edit" icon="mdi-delete" @click="showDialog = true"></v-btn>
+              <!-- <v-btn v-else icon="mdi-arrow-right" :to="navigate(party.status)"></v-btn> -->
+            </template>
+            <pop-up-dialog
+              v-if="showDialog"
+              title="Confirm Delete"
+              :info-text="'Do you really want to delete the party: ' + party.name"
+              primary-text="Delete"
+              @on-primary="deletePartyByID(party.id)"
+              @on-secondary="showDialog = false"
+            ></pop-up-dialog>
           </v-list-item>
         </v-list>
       </v-card>
@@ -110,7 +132,7 @@
 
     <v-row>
       <v-col>
-        <spot-button to="/party/create" :primary="true" title="NEW PARTY" />
+        <spot-button primary to="/party/create" title="NEW PARTY" />
       </v-col>
     </v-row>
     <v-spacer />
