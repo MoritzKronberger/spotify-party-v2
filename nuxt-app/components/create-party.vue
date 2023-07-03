c89d8c767516d05a100b0855d9d67db48b53658c
Martin Kohnle <martin.kohnle@hs-augsburg.de>
2023-07-03 10:48:14 +0200

    Update create-party

--- nuxt-app/components/create-party.vue
+++ nuxt-app/components/create-party.vue
@@ -2,12 +2,13 @@
   import SpotButton from '~/components/spot-button.vue'
   import { MimeType } from '~/types/trpc'
   import { mimeTypes } from '~/db/schema/image'
-
+  import { fileToBase64 } from '~/utils/imageHelper'
   // Get tRPC client
   const { $client } = useNuxtApp()
   // Get router
   const router = useRouter()
 
+  // Get user and session-code
   const code = await useSessionCode()
   const user = await useUser(code)
 
@@ -19,9 +20,6 @@
     name: ref(''),
     description: ref(''),
   }
-
-  const base64Blob = ref<string | undefined>(undefined)
-  const mimeType = ref<MimeType>()
 
   // Define prop for vuetify-image-validation
   // Ref: https://vuetifyjs.com/en/api/v-file-input/#props-validation-value
@@ -47,31 +45,8 @@
   // New reference rules prop -> reactive
   const rules = toRef(props, 'rules')
 
-  // File to Blob(Base-64) Conversion
-  // FileReader API: https://developer.mozilla.org/en-US/docs/Web/API/FileReader
-  // FileReader.readAsDataURL(): https://developer.mozilla.org/en-US/docs/Web/API/FileReader/readAsDataURL
-  const fileToBase64 = (file: File | undefined): Promise<string> => {
-    return new Promise<string>((resolve, reject) => {
-      const reader = new FileReader()
-      reader.onload = () => {
-        if (typeof reader.result === 'string') {
-          const base64 = reader.result.split(',')[1]
-          if (base64) {
-            const decoder = new TextDecoder('utf-8')
-            const buffer = decoder.decode(Uint8Array.from(atob(base64), (c) => c.charCodeAt(0)))
-            resolve(buffer)
-          } else {
-            reject(new Error('Failed to convert file to Base64'))
-          }
-        } else {
-          reject(new Error('Failed to read file'))
-        }
-      }
-      reader.onerror = reject
-      reader.readAsDataURL(file!)
-    })
-  }
-
+  const base64Blob = ref<string | undefined>(undefined)
+  const mimeType = ref<MimeType>()
   const createParty = async () => {
     if (file.value[0]) {
       base64Blob.value = await fileToBase64(file.value[0])
@@ -94,6 +69,8 @@
       })
   }
 
+  // Check for isUpdate -> Edit Mode
+  // Fetch party information
   if (props.isUpdate) {
     await $client.party.getPartyByCode
       .query({ code })
