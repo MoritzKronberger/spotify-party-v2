a7462780b751aacb7b9ba7884bb11c8f7925769a
Martin Kohnle <martin.kohnle@hs-augsburg.de>
2023-07-03 11:14:27 +0200

    Remove log

--- nuxt-app/composables/useSessionCode.ts
+++ nuxt-app/composables/useSessionCode.ts
@@ -2,7 +2,6 @@ export default async function useSessionCode() {
   const router = useRouter()
   const route = useRoute()
   const code = route.query.code?.toString() ?? undefined
-  console.log(code)
   /* Pushback */
   if (code === undefined) {
     await router.push({ path: '/party/join-party' })
