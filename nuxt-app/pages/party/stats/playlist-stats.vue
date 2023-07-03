d28769f46da1fd782c5a087bdfa606657b895e80
Martin Kohnle <martin.kohnle@hs-augsburg.de>
2023-07-03 14:43:02 +0200

    Update playlist-stats to expansion-panel

--- nuxt-app/pages/party/stats/playlist-stats.vue
+++ nuxt-app/pages/party/stats/playlist-stats.vue
@@ -6,40 +6,44 @@
 <template>
   <v-container class="fill-height flex-column">
     <v-spacer></v-spacer>
-    <v-card elevation="2" min-width="90vw">
-      <div class="d-flex flex-nowrap justify-space-between">
-        <div>
-          <v-card-title>Party playlist</v-card-title>
-          <v-card-subtitle class="py-1">{{ partySession.playlist.value?.name }}</v-card-subtitle>
-          <v-card-actions class="pl-4">
-            <v-btn color="primary" variant="elevated" :href="partySession.playlist.value?.shareURL" target="_blank"
-              >Open in Spotify</v-btn
-            >
-          </v-card-actions>
-        </div>
-        <div class="d-flex flex-column justify-center">
-          <v-avatar class="ma-3" rounded="0" size="10vh">
-            <v-img :src="partySession.playlist.value?.images[0]?.url"></v-img>
-          </v-avatar>
-        </div>
-      </div>
-      <v-card-item>
-        <v-list lines="one" style="height: 54vh" class="overflow-y-auto mx-auto">
-          <v-list-item
-            v-for="track in partySession.playlist.value?.tracks"
-            :key="track.id"
-            :title="track.name"
-            :subtitle="'by ' + track.artists?.map((artist) => artist.name).join(', ')"
-          >
-            <template #prepend>
-              <v-avatar class="ma-3" rounded="0" size="7vh">
-                <v-img :src="track.images?.[0]?.url"></v-img>
+    <v-row>
+      <v-col>
+        <v-expansion-panels>
+          <v-expansion-panel>
+            <template #title>
+              <v-avatar rounded="0" size="10vh">
+                <v-img :src="partySession.playlist.value?.images[0]?.url"></v-img>
               </v-avatar>
+              <div class="d-flex flex-nowrap justify-space-between">
+                <div>
+                  <v-card-title>Party playlist</v-card-title>
+                  <v-card-subtitle class="py-1">{{ partySession.playlist.value?.name }}</v-card-subtitle>
+                </div>
+              </div>
             </template>
-          </v-list-item>
-        </v-list>
-      </v-card-item>
-    </v-card>
+            <template #text>
+              <v-card-item class="px-0">
+                <v-list lines="one" style="height: 54vh" class="overflow-y-auto mx-auto py-0">
+                  <v-list-item
+                    v-for="track in partySession.playlist.value?.tracks"
+                    :key="track.id"
+                    :title="track.name"
+                    :subtitle="'by ' + track.artists?.map((artist) => artist.name).join(', ')"
+                    class="px-0"
+                  >
+                    <template #prepend>
+                      <v-avatar class="ma-3" rounded="0" size="7vh">
+                        <v-img :src="track.images?.[0]?.url"></v-img>
+                      </v-avatar>
+                    </template>
+                  </v-list-item>
+                </v-list>
+              </v-card-item>
+            </template>
+          </v-expansion-panel>
+        </v-expansion-panels>
+      </v-col>
+    </v-row>
     <v-spacer></v-spacer>
   </v-container>
 </template>
