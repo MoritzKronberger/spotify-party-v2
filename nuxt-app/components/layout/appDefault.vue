5a3a89afdc74a1097e02694c04101baf8d905ee5
Martin Kohnle <martin.kohnle@hs-augsburg.de>
2023-07-03 12:07:47 +0200

    Update appDefault

--- nuxt-app/components/layout/appDefault.vue
+++ nuxt-app/components/layout/appDefault.vue
@@ -8,16 +8,9 @@
   }>()
 
   const router = useRouter()
-  const route = useRoute()
-  const code = route.query.code ?? ''
-  const user = await useUser(code.toString())
 
-  const routeBack = (location: string | undefined) => {
-    if (!user.isHost && location) {
-      router.push({ path: location })
-    } else {
-      router.go(-1)
-    }
+  const routeBack = () => {
+    router.go(-1)
   }
 </script>
 
@@ -25,12 +18,7 @@
   <v-theme-provider theme="defaultTheme" with-background>
     <v-app class="gradient-background">
       <v-app-bar v-if="!props.hideNavigation" color="primary">
-        <v-app-bar-nav-icon icon="mdi-arrow-left" @click="routeBack(routeLocation)" />
-        <template v-if="showOption" #append>
-          <v-btn v-if="user.isHost" icon :to="`/party/edit-party?code=${route.query.code}`">
-            <v-icon>mdi-cog</v-icon>
-          </v-btn>
-        </template>
+        <v-app-bar-nav-icon icon="mdi-arrow-left" @click="routeBack" />
       </v-app-bar>
       <!-- App content goes here -->
       <v-main>
