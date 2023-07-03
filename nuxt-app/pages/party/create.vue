2e53e8e216fcca77dbaabfaa9a9b725fc5becabb
Martin Kohnle <martin.kohnle@hs-augsburg.de>
2023-07-03 15:20:23 +0200

    Update create-party auth

--- nuxt-app/pages/party/create.vue
+++ nuxt-app/pages/party/create.vue
@@ -1,4 +1,8 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+  definePageMeta({
+    middleware: ['auth'],
+  })
+</script>
 <template>
   <CreateParty :is-update="false"></CreateParty>
 </template>
