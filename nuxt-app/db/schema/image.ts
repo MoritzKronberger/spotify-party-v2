66559e8169965530e077f1e674c320254a938edb
Moritz Kronberger <moritz.kronberger99@gmail.com>
2023-07-04 17:27:03 +0200

    Add index for foreign key columns

--- nuxt-app/db/schema/image.ts
+++ nuxt-app/db/schema/image.ts
@@ -1,13 +1,19 @@
-import { mysqlEnum, mysqlTable, mediumtext } from 'drizzle-orm/mysql-core'
+import { mysqlEnum, mysqlTable, mediumtext, index } from 'drizzle-orm/mysql-core'
 import { nanoId } from '~/utils/nanoId/drizzle'
 import { mimeTypes } from '~/utils/image'
 
 /**
  * Drizzle schema for image.
  */
-export default mysqlTable('image', {
-  id: nanoId('id').primaryKey(),
-  userId: nanoId('user_id').notNull(),
-  base64Blob: mediumtext('base64_blob').notNull(),
-  mimeType: mysqlEnum('mime_type', mimeTypes).notNull(),
-})
+export default mysqlTable(
+  'image',
+  {
+    id: nanoId('id').primaryKey(),
+    userId: nanoId('user_id').notNull(),
+    base64Blob: mediumtext('base64_blob').notNull(),
+    mimeType: mysqlEnum('mime_type', mimeTypes).notNull(),
+  },
+  (image) => ({
+    userIndex: index('user_index').on(image.userId),
+  })
+)
