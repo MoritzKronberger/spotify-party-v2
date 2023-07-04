6047e049c29719420b492a1e70758a79232cdfa7
Moritz Kronberger <moritz.kronberger99@gmail.com>
2023-07-04 17:28:02 +0200

    Remove unused table

--- nuxt-app/db/schema/index.ts
+++ nuxt-app/db/schema/index.ts
@@ -1,10 +1,9 @@
 import image from './image'
 import party from './party'
-import songs from './songs'
 import user from './user'
 
 /**
  * Register all schema components by exporting them from here.
  * (For compatibility with Drizzle `db-push`)
  */
-export { songs, image, party, user }
+export { image, party, user }
